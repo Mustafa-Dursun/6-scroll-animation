736bac6d2c1526f5932dcf97c258935ed2e2ed09
Mustafa Dursun <dursunmustafainfo@gmail.com>
2023-06-01 14:11:07 +0300

    deneme

---
+++ app.js
@@ -0,0 +1,4 @@
+ScrollReveal().reveal('.headline', {delay:1000});
+ScrollReveal().reveal('.ani1', {delay:250});
+ScrollReveal().reveal('.ani2', {delay:500});
+ScrollReveal().reveal('.ani3', {delay:1000});
